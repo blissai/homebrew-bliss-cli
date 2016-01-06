50d1fe8ce5e75ec0b851ceb0ca7f5052bf44ef80
Michael Sive <mikesive@gmail.com>
2016-01-06 14:31:58 +1000

    remove file search from script install

--- Formula/bliss.rb
+++ Formula/bliss.rb
@@ -8,7 +8,7 @@ class Bliss < Formula
 
   def install
     lib.install Dir['lib/**']
-    prefix.install Dir['scripts/**']
+    prefix.install Dir['scripts']
     prefix.install 'blisscollector.rb'
     prefix.install 'Dockerfile'
     prefix.install 'Gemfile'
