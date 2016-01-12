552038745f45ffd5d73099762b27bad6caa3efc4
Michael Sive <mikesive@gmail.com>
2016-01-12 14:27:23 +1000

    v 1.0.26

--- Formula/bliss.rb
+++ Formula/bliss.rb
@@ -1,10 +1,10 @@
 class Bliss < Formula
-  VERSION = '1.0.25'
+  VERSION = '1.0.26'
   version "#{VERSION}"
   desc 'Bliss Enterprise Tool for local repository analysis.'
   homepage 'https://github.com/founderbliss/docker'
   url "https://github.com/founderbliss/bliss-cli/archive/#{VERSION}.tar.gz"
-  sha256 'ba33baedf826019fc28b054ef02d62993332886e4b5c430c8a94d865454a3dbe'
+  sha256 '36ee6ee09d7efb7246fab608bc1ca97b50ccb9b73f03d741ff0ca1c83696d7fd'
   head 'https://github.com/founderbliss/bliss-cli.git'
 
   def install
